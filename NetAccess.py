3e91e9c997a488777b01ae6deb093d8c7393f2df
Ajeet E S <MildlySane>
2023-05-27 13:03:18 +0530

    Commented the code and made minor changes

--- NetAccess.py
+++ NetAccess.py
@@ -1,13 +1,14 @@
-from time import *
+# Importing Packages
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+# Roll Number and Password
 roll_no="YOUR ROLL NO"
 passwd="YOUR PASSWORD"
 
-service = Service(executable_path="C://Users//ajeet//OneDrive//Desktop//NetAccess//ChromeDriver//chromedriver.exe")
+service = Service(executable_path="PATH TO CHROME DRIVER")
 driver = webdriver.Chrome(service=service)
 
 driver.get("http://netaccess.iitm.ac.in/account/login")
@@ -22,33 +23,27 @@ unsafebut=driver.find_element(By.ID,"proceed-link")
 unsafebut.click()
 """
 
-
+# Fill in the Username Field
 elem=driver.find_element(By.NAME,'userLogin')
 elem.clear()
 elem.send_keys(roll_no)
 
-
-
+#Fill in the Password Field
 password=driver.find_element(By.NAME,'userPassword')
 password.clear()
 password.send_keys(passwd)
 
-
-
+#Press Buttons:
 submit=driver.find_element(By.NAME,'submit')
 submit.click()
 
-
-
 driver.find_element(By.LINK_TEXT,"approve").click()
-
 
 but=driver.find_element(By.ID,"radios-1").click()
 
-
 auth=driver.find_element(By.NAME,"approveBtn").click()
 
-
+#Close the webdriver instance
 driver.close()
 
 
